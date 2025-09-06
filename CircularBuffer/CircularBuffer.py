0e5ba401f7c293497e94cc31e1555ff9edf4e477
Ondrej Bartik <onbartik@gmail.com>
2025-09-06 23:16:32 +0200

    Create basic structure of circular buffer.
    Add private methods for incrementing the write/read pointers. Add public method flush() and get_occupancy().

---
+++ CircularBuffer/CircularBuffer.py
@@ -0,0 +1,79 @@
+from enum import Enum
+
+import numpy as np
+
+
+class CircBuffFlags(Enum):
+    EMPTY = 0
+    FULL = 1
+    NONEMPTY = 2
+
+
+class WriteOverFlowMode(Enum):
+    """Modes for write overflow handling
+        1) LIMIT - When overflow occurs, new data write is prohibited.
+        2) FLOW - When overflow occurs, new data write is permited."""
+
+    LIMIT = 0
+    FLOW = 1
+
+
+class CircBuff:
+    instance_count = 0
+
+    def __init__(self, width: int, depth: int, data_type: np.dtype, write_overflow_mode: WriteOverFlowMode):
+        CircBuff.instance_count += 1
+        self.width = width
+        self.depth = depth
+        self.data = np.zeros((self.depth, self.width), dtype=data_type)
+        self.read_ptr = 0
+        self.write_ptr = 0
+        self.write_overflow_mode = write_overflow_mode
+        self.buffer_state = CircBuffFlags.EMPTY
+
+    def __inc_write_ptr(self):
+        self.write_ptr += 1
+        if self.write_ptr > self.depth:
+            self.write_ptr = 0
+
+        if self.write_ptr == self.read_ptr:
+            """If write_ptr catches up the read_ptr, buffer is full."""
+            self.buffer_state = CircBuffFlags.FULL
+        else:
+            self.buffer_state = CircBuffFlags.NONEMPTY
+
+        return self.buffer_state
+
+    def __inc_read_ptr(self):
+        self.read_ptr += 1
+        if self.read_ptr > self.depth:
+            self.read_ptr = 0
+
+        if self.read_ptr == self.write_ptr:
+            """If read_ptr catches up the write_ptr and the buffer was not overflowed, buffer is empty.
+               If it was overflowed, the buffer is only non-empty"""
+            if self.buffer_state == CircBuffFlags.FULL:
+                self.buffer_state = CircBuffFlags.NONEMPTY
+            else:
+                self.buffer_state = CircBuffFlags.EMPTY
+
+        return self.buffer_state
+
+    def get_occupancy(self):
+        if self.buffer_state == CircBuffFlags.FULL:
+            return self.depth
+        else:
+            return self.write_ptr - self.read_ptr
+
+    def flush(self):
+        self.write_ptr = 0
+        self.read_ptr = 0
+        self.buffer_state = CircBuffFlags.EMPTY
+
+        return CircBuffFlags.EMPTY
+
+    def write_single(self, data: np.ndarray):
+        ...
+
+    def read_single(self):
+        ...
